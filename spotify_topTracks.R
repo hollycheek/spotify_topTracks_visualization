62f18155e97c688b4b6ebacbd54d0647b1204813
Ananya Haravu <Ananya@169-231-153-137.wireless.ucsb.edu>
2017-10-30 20:51:08 -0700

    CLEAN DATA

--- spotify_topTracks.R
+++ spotify_topTracks.R
@@ -1,28 +1,49 @@
-library(readr)
-data <- read.csv(file='song-data-unique.csv', header = TRUE, sep = ',')
-View(data)
-spotifyDataset <- read.csv(file='song-data-unique.csv', header = TRUE, sep = ',')
-View(spotifyDataset)
-spotifyDataset$time_signature=NULL
-install.packages('ggplot2')
-install.packages('plotly')
+setwd('/Users/Ananya/myProjects/spotify_topTracks_visualization')
 
+#Read csv file and create dataset
+spotifyDataset <- read.csv(file='song_data_cleaned.csv', header = TRUE, sep = ',')
+spotifyDatasetOld <- read.csv(file='song-data-unique.csv', header = TRUE, sep = ',')
+View(spotifyDataset)
+View(spotifyDatasetOld)
+
+#delete time signature column because did not seem useful for analysis
+spotifyDataset$time_signature=NULL
+spotifyDatasetOld$time_signature=NULL
+
+#install.packages('ggplot2')
+#install.packages('plotly')
+#install.packages('ISLR')
+#library(ISLR)
+library(tidyverse)
 library(ggplot2)
 library(plotly)
-install.packages('ISLR')
-library(ISLR)
-data(spotifyDataset)
+
+#Loads data into data frame
 spotify_dt <- data.frame(spotifyDataset)
-spotify_dt[1]
-spotify_dt[,1, with = FALSE]
-ggplot(data = spotify_dt, aes(x=danceability, y=popularity))
-data = spotify_dt
-ggplot(data = spotify_dt, aes(x=danceability, y=popularity)) + geom_point()
+spotify_dt_old <- data.frame(spotifyDatasetOld)
+
+
+#Merge to new dataset
+#?merge
+spotify_dt_new <- merge.data.frame(spotify_dt_old, spotify_dt, by.x = 'songid', by.y = 'songid')
+View(spotify_dt_new)
+
+#Visualization of danceability vs. popularity by category 
 ggplot(data = spotify_dt, 
        aes(x = danceability, 
-           y = popularity)) +
-  geom_point(col = 'turquoise4') + 
+           y = popularity,
+           col = category)) +
+  geom_point() + 
   ggtitle('Scatter Plot: Danceability Vs. Popularity') +
   labs(x = 'Danceability', 
        y = 'Popularity') + 
-  theme_minimal()
+  theme_minimal() + 
+  facet_wrap(~category)
+
+#???
+spotify_dt %>% 
+  filter(category == 'Pop') %>%
+  select(danceability)
+
+
+
