6cdf22bb9070629a22391b3dd1ba8037ee7a733b
hollycheek <hollycheekm@gmail.com>
2017-11-16 20:42:27 -0800

    updated

--- spotify_topTracks.R
+++ spotify_topTracks.R
@@ -8,9 +8,11 @@ setwd('/Users/Ananya/myProjects/spotify_topTracks_visualization')
 install.packages('ggplot2')
 install.packages('plotly')
 install.packages('dplyr')
+install.packages('tidyverse')
 library('ggplot2')
 library('plotly')
 library('dplyr')
+library('tidyverse')
 
 #Read csv file and create dataset
 spotifyDatasetOld <- read.csv(file='originalData.csv', header = TRUE, sep = ',')
@@ -40,6 +42,9 @@ write.csv(completeData, 'cleanedData.csv', row.names=FALSE)
 
 #Delete time signature column because did not seem useful for analysis
 completeData$time_signature=NULL
+
+#change all integer data into numeric
+completeData <- lapply(completeData, as.numeric)
 
 #Visualization of danceability vs. popularity by category 
 ggplot(data = completeData, 
@@ -126,14 +131,33 @@ ggplot(data = completeData,
            y = popularity,
            col = category)) +
   geom_point() + 
-  #LOOK AT SPREAD
   geom_jitter() +
   geom_smooth(method = 'lm') +
   ggtitle('Scatter Plot: Category Vs. Popularity') +
   labs(x = 'Category', 
        y = 'Popularity') + 
   theme_minimal()
-
+  #look at spread use group by function to group each category/genre
+  #dplyr::group_by(completeData, category) 
+  #Group data by each category/genre
+  EDM <- dplyr::filter(completeData, completeData$category== 'Electronic/Dance')
+  HipHop <- dplyr::filter(completeData, completeData$category== 'Hip-Hop')
+  Pop <- dplyr::filter(completeData, completeData$category== 'Pop')
+  Rock <- dplyr::filter(completeData, completeData$category== 'Rock')
+  Indie <- dplyr::filter(completeData, completeData$category== 'Indie')
+  Jazz <- dplyr::filter(completeData, completeData$category== 'Jazz')
+  Metal <- dplyr::filter(completeData, completeData$category== 'Metal')
+  RandB <- dplyr::filter(completeData, completeData$category== 'R&B')
+  Romance <- dplyr::filter(completeData, completeData$category== 'Romance')
+  Soul <- dplyr::filter(completeData, completeData$category== 'Soul')
+  #Look at spread of popularity between each category/genre
+  row.names=c('Category/Genre', 'Stardard Deviation','Variance', 'IQR')
+  categoryCol <- levels(completeData$category)
+  standDevCol <- c(sd(EDM$popularity), sd(HipHop$popularity),sd(Pop$popularity),sd(Rock$popularity),sd(Indie$popularity),sd(Jazz$popularity),sd(Metal$popularity),sd(RandB$popularity),sd(Romance$popularity),sd(Soul$popularity))
+  varCol <- c(var(EDM$popularity), var(HipHop$popularity),var(Pop$popularity),var(Rock$popularity),var(Indie$popularity),var(Jazz$popularity),var(Metal$popularity),var(RandB$popularity),var(Romance$popularity),var(Soul$popularity))
+  iqrCol <- c(IQR(EDM$popularity), IQR(HipHop$popularity),IQR(Pop$popularity),IQR(Rock$popularity),IQR(Indie$popularity),IQR(Jazz$popularity),IQR(Metal$popularity),IQR(RandB$popularity),IQR(Romance$popularity),IQR(Soul$popularity))
+  #MAKE DATA FRAME AND VISUALIZE
+  
 #???
 spotify_dt %>% 
   filter(category == 'Pop') %>%
